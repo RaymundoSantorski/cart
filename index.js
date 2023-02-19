1fb5acd12360835e86a296978c3f4fa5222ffb86
Raymundo Mateos <rayma9829@gmail.com>
2023-02-18 22:31:30 -0600

    [ADD] funcion showCart para eliminar la clase hide del contenedor del carrito y así mostrarlo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const closeButton = document.getElementById('close_button');
 const cartContainer = document.querySelector('.cart_container');
+const cartButton = document.querySelector('.cart_button');
 
 const hideCart = () => {
     cartContainer.classList.add('hide');
 }
 
+const showCart = () => {
+    cartContainer.classList.remove('hide');
+}
+
 closeButton.addEventListener('click', hideCart);
+cartButton.addEventListener('click', showCart);
