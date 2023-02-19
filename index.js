ed06edd0bbee9f1fe6d0259497935cdd626d0927
Raymundo Mateos <rayma9829@gmail.com>
2023-02-18 22:18:00 -0600

    [ADD] al hacer click en el botón de cerrar carrito, al contenedor del mismo se le agrega la clase hide, para eliminarlo del DOM

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const closeButton = document.getElementById('close_button');
+const cartContainer = document.querySelector('.cart_container');
 
-console.log(closeButton);
+const hideCart = () => {
+    cartContainer.classList.add('hide');
+}
+
+closeButton.addEventListener('click', hideCart);
